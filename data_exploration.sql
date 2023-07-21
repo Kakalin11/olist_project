1c6edcd447b4c93102ae3a455b2d0098b3a8d2cb
Kakalin11 <134309914+Kakalin11@users.noreply.github.com>
2023-07-21 23:53:20 +0300

    Add files via upload

--- data_exploration.sql
+++ data_exploration.sql
@@ -2,11 +2,17 @@ use olist;
 
 select * from order_payments;
 
-select * from orders;
+
+-- How many orders are there for each status?
+
+select  order_status, count(*) as total_orders, count(*)/(select count(*) from olist.orders)*100 as total_orders_percentage from orders
+group by order_status
+order by 2 desc;
 
 select * from customers;
 
 
+-- Which are the top 10 cities that most orders delivered?
 
 select B.customer_city,Count(A.order_id) from orders A
 JOIN customers B on A.customer_id = B.customer_id
@@ -14,3 +20,11 @@ where order_status='delivered'
 group by B.customer_city 
 order by 2 desc
 limit 10;
+
+select * from orders;
+select YEAR(B.order_purchase_timestamp),A.payment_type,sum(A.payment_value) from olist.order_payments A
+JOIN orders B on A.order_id=B.order_id 
+group by YEAR(B.order_purchase_timestamp),A.payment_type
+order by 1 desc;
+
+select * from olist.sellers;
